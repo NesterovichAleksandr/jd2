898b5b3f4d4f968e8a1fb1baf13059a8b550bec3
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2018-12-01 15:48:30 +0300

    test session

---
+++ hello-hibernate/src/test/java/by/pvt/util/HibernateUtilTest.java
@@ -0,0 +1,32 @@
+package by.pvt.util;
+
+import org.hibernate.Session;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HibernateUtilTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getInstance() {
+        assertNotNull(HibernateUtil.getInstance());
+    }
+
+    @Test
+    public void getSession() {
+        Session session = HibernateUtil.getInstance().getSession();
+        assertNotNull(session);
+
+        assertEquals(session, HibernateUtil.getInstance().getSession());
+    }
+}
