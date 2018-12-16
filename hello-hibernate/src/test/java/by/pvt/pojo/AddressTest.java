82f2b6583bcc71c6c9541a526ac3362d2b98b1b0
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2018-12-16 10:27:52 +0300

    correct test

--- hello-hibernate/src/test/java/by/pvt/pojo/AddressTest.java
+++ hello-hibernate/src/test/java/by/pvt/pojo/AddressTest.java
@@ -1,6 +1,5 @@
 package by.pvt.pojo;
 
-import by.pvt.dao.DaoImpl;
 import by.pvt.util.HibernateUtil;
 import org.hibernate.Session;
 import org.junit.After;
@@ -11,27 +10,33 @@ import static org.junit.Assert.*;
 
 public class AddressTest {
 
-    private DaoImpl<Address> dao;
+    private Session session;
 
     @Before
-    public void setUp() {
-        dao = new DaoImpl<>(Address.class);
-        DaoImpl.isTestInstance = true;
+    public void setUp() throws Exception {
+        session = HibernateUtil.getInstance().getTestSession();
     }
 
     @Test
     public void getId() {
         Address address = new Address("Minsk", "Stroiteley", "5", 24);
-        Session session = HibernateUtil.getInstance().getTestSession();
-        session.saveOrUpdate(address);
-
-        assertEquals(4L, address.getId());
-        session.delete(address);
+        try {
+            session.beginTransaction();
+            session.saveOrUpdate(address);
+            assertTrue(address.getId() > 0);
+            session.delete(address);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
     }
 
     @After
-    public void tearDown() {
-        DaoImpl.isTestInstance = false;
-        dao = null;
+    public void tearDown() throws Exception {
+        if (session != null && session.isOpen()) {
+            session.close();
+            session = null;
+        }
     }
 }
